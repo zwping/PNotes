9234c1ae45d394bacd3c3baf9420e1974c45c40f
zwping <1101558280@qq.com>
2017-12-27 15:14:49 +0800

    up

--- AndroidNotes/imageloader/src/main/java/win/zwping/imageloader/MainActivity.java
+++ AndroidNotes/imageloader/src/main/java/win/zwping/imageloader/MainActivity.java
@@ -1,7 +1,25 @@
 package win.zwping.imageloader;
 
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.load.resource.gif.GifDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.target.Target;
+import com.bumptech.glide.request.transition.Transition;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Random;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +27,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+        System.out.println("Max memory is " + maxMemory + "KB");
+
+//        Glide.with(this).asGif().load("123").apply(new RequestOptions().dontTransform().override(Target.SIZE_ORIGINAL,Target.SIZE_ORIGINAL).transform()).into(new ImageView(this));
+
+        Set<Integer> sets = new HashSet<>();
+        for (int i = 0; i < 10; i++) {
+            sets.add(new Random().nextInt(5));
+        }
+
+        Iterator iterator = sets.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
     }
 }
--- AndroidNotes/imageloader/src/main/java/win/zwping/imageloader/glide/GlideModule.java
+++ AndroidNotes/imageloader/src/main/java/win/zwping/imageloader/glide/GlideModule.java
@@ -6,21 +6,26 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.Registry;
 import com.bumptech.glide.load.DecodeFormat;
+import com.bumptech.glide.load.engine.cache.ExternalPreferredCacheDiskCacheFactory;
 import com.bumptech.glide.module.AppGlideModule;
 import com.bumptech.glide.request.RequestOptions;
 
 /**
  * <p>describe：
  * <p>    note：Glide默认加载Bitmap格式为RGB_565，Picasso默认加载Bitmap格式为ARGB_8888
- * <p>    note: Glide重写{@link AppGlideModule}也可以加载ARGB_8888格式的Bitmap，需要使用<meta-data>标签初始化
+ * <p>    note: Glide重写{@link AppGlideModule}也可以加载ARGB_8888格式的Bitmap、将缓存地址更换至SD卡，并将缓存大小调整为原来的2倍(500M)，同时需要使用<meta-data>标签初始化
  * <p>    note:
  * <p> @author：zwp on 2017/12/26 0026 mail：1101558280@qq.com web: http://www.zwping.win </p>
  */
 public class GlideModule extends AppGlideModule {
+
+    public static final int DISK_CACHE_SIZE = 500 * 1024 * 1024;
+
     @Override
     public void applyOptions(Context context, GlideBuilder builder) {
         super.applyOptions(context, builder);
-        builder.setDefaultRequestOptions(new RequestOptions().format(DecodeFormat.PREFER_ARGB_8888));
+        builder.setDiskCache(new ExternalPreferredCacheDiskCacheFactory(context, DISK_CACHE_SIZE)); //更换缓存地址及扩充缓存大小
+        builder.setDefaultRequestOptions(new RequestOptions().format(DecodeFormat.PREFER_ARGB_8888)); //更换编码格式
     }
 
     @Override
